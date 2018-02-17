ee830b6781b2d97f90392ba0951c428b7ef53364
sk-g <sgouda@ucsc.edu>
2018-02-17 12:49:00 -0800

    files ready for supervised

--- data/PCH/paths/table_to_paths.py
+++ data/PCH/paths/table_to_paths.py
@@ -18,9 +18,8 @@ class pathExtractor(object):
 		for i in range(len(self._lines)):
 			self._originalPaths.append(str(''.join([str(j) for j in self._lines[i].strip().split('\t')[-1]])))
 		return self._originalPaths
+
 	def getInts(self):
-		
-		#if len(self._orignalPaths) == 0 :
 		if not self._originalPaths:
 			print("run the getPaths method first")
 			return
@@ -29,24 +28,34 @@ class pathExtractor(object):
 			#[int(i) for i in obj._originalPaths[100].split(' ')]
 			self.__IntPaths.append([int(j) for j in arr[i].split('\t')])
 		return self.__IntPaths
-	def misconfiguredPaths(self,strx):
+
+	def misconfiguredPaths(self):
 		if len(self._originalPaths) == 0:
 			print("run the getPaths method first")
-		pass
-		#should take entire file or line by line?
+		how_many = int(0.1*len(self._originalPaths))
+		random_paths = np.random.choice(self._originalPaths,how_many,replace = False)
+		for i in range(len(random_paths)):
+			splits = random_paths[i].split(' ')
+			if len(splits) >=2:
+				self._misconfiguredPaths.append(' '.join(random.sample(splits,len(splits))))
+		print(self._misconfiguredPaths)
+
 	def hijackedPaths(self):
 		if len(self._originalPaths) == 0:
 			print("run the getPaths method first")
-		random_paths = np.random.choice(self._originalPaths,3,replace = False)
-		for i in random_paths:
-			print(i,len(i.split(' ')))
-		#print(random_selection)
-		pass
-	def variedLengths(self,strx):
+		if len(self._originalPaths) == 0:
+			print("run the getPaths method first")
+		how_many = int(0.1*len(self._originalPaths))
+		random_paths = np.random.choice(self._originalPaths,how_many,replace = False)
+		#print(how_many)
+		for i in range(len(random_paths)):
+			if len(random_paths[i].split(' '))> 3:
+				self._hijackedPaths.append(random_paths[i]+" "+self._originalPaths[100].split(' ')[1])
+	def variedLengths(self):
 		if len(self._originalPaths) == 0:
 			print("run the getPaths method first")
 		pass
-	def showAllPaths(self,strx):
+	def showAllPaths(self):
 		#call this after finishing up above three functions
 		#otherwise,legnth overflow
 		# for i in range
@@ -59,7 +68,18 @@ class pathExtractor(object):
 		#all arrays
 		if len(self._originalPaths) == 0:
 			print("run the getPaths method first")
-		pass
+		#write with same filename + fake
+		#all arrays
+		if len(self._hijackedPaths) == 0:
+			self.hijackedPaths()
+		if len(self._misconfiguredPaths) == 0:
+			self.misconfiguredPaths()
+		self.__combined__ = self._hijackedPaths + self._misconfiguredPaths
+		f = open(self.fname+"_f.txt",'a')
+		for i in self.__combined__:
+			f.write(i+"\n")
+		f.close()
+		return
 
 #obj = pathExtractor('24012018_.txt')
 #obj.getInts() < - correctly fetches error asking to call getPaths first
@@ -67,3 +87,7 @@ class pathExtractor(object):
 #tw = open('24012018.txt','w')
 #for i in range(len(obj._originalPaths)):
 #	tw.write(obj._originalPaths[i]+"\n")
+obj = pathExtractor('11012018.txt')
+retPaths = obj.getPaths()
+hjp = obj.hijackedPaths()
+obj.writePaths()
